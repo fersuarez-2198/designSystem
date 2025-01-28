c1f8dcccba2697801827bdc0826ad0d3172ea5ac
fersuarez-2198 <fersuarez.2198@gmail.com>
2025-01-28 16:37:26 -0500

    Configuración popups

--- design_system/dist/components/Popup/Popup.js
+++ design_system/dist/components/Popup/Popup.js
@@ -4,38 +4,63 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 require("./Popup.css");
 var _Close = _interopRequireDefault(require("@mui/icons-material/Close"));
 var _Button = _interopRequireDefault(require("../Button/Button"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function (e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
 const PopupDS = _ref => {
   let {
     size,
     showCloseButton,
     title,
     body,
-    // Aquí seguimos usando `body` pero permitiendo que acepte JSX
     onAccept,
     onReject,
     acceptText,
     rejectText,
-    showRejectButton
+    showRejectButton,
+    isVisible,
+    // Prop que controla la visibilidad del modal
+    onClose // Prop para manejar el cierre desde el componente padre
   } = _ref;
-  return /*#__PURE__*/_react.default.createElement("div", {
-    className: "popup-overlay"
+  const [isAnimating, setIsAnimating] = (0, _react.useState)(false);
+  (0, _react.useEffect)(() => {
+    if (isVisible) {
+      // Cuando se abre el popup, activa la animación
+      setIsAnimating(true);
+    } else {
+      // Si se cierra, espera la duración de la animación antes de desactivar
+      const timer = setTimeout(() => {
+        setIsAnimating(false);
+      }, 300); // Duración de la transición en CSS (0.3s)
+
+      return () => clearTimeout(timer);
+    }
+  }, [isVisible]);
+  const handleOverlayClick = e => {
+    if (e.target.className.includes("popup-overlay")) {
+      onClose(); // Llamamos a la función de cierre desde el padre
+    }
+  };
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, (isVisible || isAnimating) && /*#__PURE__*/_react.default.createElement("div", {
+    className: "popup-overlay ".concat(isVisible ? "show" : ""),
+    onClick: handleOverlayClick
   }, /*#__PURE__*/_react.default.createElement("div", {
-    className: "popup ".concat(size === "large" ? "popup-large" : "popup-medium")
+    className: "popup ".concat(size === "large" ? "popup-large" : "popup-medium", " ").concat(isVisible ? "show" : ""),
+    onClick: e => e.stopPropagation()
   }, showCloseButton && /*#__PURE__*/_react.default.createElement("button", {
     className: "popup-close",
-    onClick: onReject || onAccept
+    onClick: onClose
   }, /*#__PURE__*/_react.default.createElement(_Close.default, {
     className: "toast_closeButton"
   })), title && /*#__PURE__*/_react.default.createElement("div", {
     className: "popup-title"
   }, title), /*#__PURE__*/_react.default.createElement("div", {
     className: "popup-body"
-  }, body), " ", /*#__PURE__*/_react.default.createElement("div", {
+  }, body), /*#__PURE__*/_react.default.createElement("div", {
     className: "popup-footer"
   }, showRejectButton && /*#__PURE__*/_react.default.createElement(_Button.default, {
     className: "button-variant8",
@@ -49,6 +74,6 @@ const PopupDS = _ref => {
     label: acceptText,
     onClick: onAccept,
     width: "120px"
-  }))));
+  })))));
 };
 var _default = exports.default = PopupDS;
--- design_system/dist/components/Popup/Popup.stories.js
+++ design_system/dist/components/Popup/Popup.stories.js
@@ -4,9 +4,12 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = exports.Medium = exports.LargeWithReject = void 0;
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 var _Popup = _interopRequireDefault(require("./Popup"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function (e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
+function _extends() { return _extends = Object.assign ? Object.assign.bind() : function (n) { for (var e = 1; e < arguments.length; e++) { var t = arguments[e]; for (var r in t) ({}).hasOwnProperty.call(t, r) && (n[r] = t[r]); } return n; }, _extends.apply(null, arguments); }
 var _default = exports.default = {
   title: "Components/Popup",
   component: _Popup.default,
@@ -25,7 +28,9 @@ var _default = exports.default = {
     title: {
       control: "text"
     },
-    // Acepta texto simple para Storybook, pero usaremos JSX en las historias
+    body: {
+      control: "text"
+    },
     acceptText: {
       control: "text"
     },
@@ -34,10 +39,25 @@ var _default = exports.default = {
     },
     rejectText: {
       control: "text"
+    },
+    isVisible: {
+      control: "boolean"
+    },
+    onClose: {
+      action: "closed"
     }
   }
 };
-const Template = args => /*#__PURE__*/_react.default.createElement(_Popup.default, args);
+const Template = args => {
+  const [isVisible, setIsVisible] = (0, _react.useState)(args.isVisible);
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+  return /*#__PURE__*/_react.default.createElement(_Popup.default, _extends({}, args, {
+    isVisible: isVisible,
+    onClose: handleClose
+  }));
+};
 const Medium = exports.Medium = Template.bind({});
 Medium.args = {
   size: "medium",
@@ -47,9 +67,11 @@ Medium.args = {
     src: "https://via.placeholder.com/150",
     alt: "Example"
   })),
-  // Ahora puedes pasar JSX directamente a `body`
   acceptText: "Aceptar",
-  onAccept: () => alert("Aceptar")
+  showRejectButton: false,
+  isVisible: true,
+  onAccept: () => alert("Aceptar"),
+  onReject: () => alert("Rechazar")
 };
 const LargeWithReject = exports.LargeWithReject = Template.bind({});
 LargeWithReject.args = {
@@ -60,9 +82,10 @@ LargeWithReject.args = {
     src: "https://via.placeholder.com/150",
     alt: "Example"
   })),
-  // Pasar contenido JSX más complejo
   acceptText: "Aceptar",
   rejectText: "Rechazar",
+  showRejectButton: true,
+  isVisible: true,
   onAccept: () => alert("Aceptar"),
   onReject: () => alert("Rechazar")
 };
